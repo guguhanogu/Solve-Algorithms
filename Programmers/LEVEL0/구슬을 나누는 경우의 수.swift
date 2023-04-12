0cf0639d8f9c2a985b5708b2f86146dddd98c402
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-04-13 03:29:48 +0900

    [Sol] Programmers/LEVEL0/구슬을 나누는 경우의 수.swift
    # [Sol] Programmers/LEVEL0/구슬을 나누는 경우의 수.swift

--- Programmers/LEVEL0/구슬을 나누는 경우의 수.swift
+++ Programmers/LEVEL0/구슬을 나누는 경우의 수.swift
@@ -9,6 +9,26 @@
 // HINT: - 서로 다른 n개 중 m개를 뽑는 경우의 수 공식은 다음과 같습니다.
 // n! / ( (n-m)! * m! )
 
+// MARK: - 2회차: Decimal
+
+import Foundation
+
+/// n의 팩토리얼을 Decimal로 return해주는 함수
+func factorial(_ n: Int) -> Decimal {
+    return n < 2 ? 1 : Decimal(n) * factorial(n - 1)
+}
+
+func solution(_ balls:Int, _ share:Int) -> Int {
+    /// Decimal을 Int형으로 casting 해준다.
+    return Int(NSDecimalNumber(decimal: factorial(balls) / (factorial(balls - share) * factorial(share))))
+}
+
+// MARK: - 1회차: Int
+/// 실패 (signal: illegal instruction (core dumped))
+/// 원인을 찾아보니
+/// 30! 부터는 Int가 표현할 수 있는 한계를 넘어버리기 때문이라고 한다.
+/// 그래서 더 큰 값을 표현할 수 있는 Decimal로 풀기로 했다.
+
 import Foundation
 
 /// n의 팩토리얼을 return해주는 함수
