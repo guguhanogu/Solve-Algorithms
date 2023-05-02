efb10a2446f083afa693afd288759d7f76887008
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-05-02 15:35:57 +0900

    [Sol] Programmers/LEVEL0/문자열의 뒤의 n글자.swift
    # Programmers/LEVEL0/문자열의 뒤의 n글자.swift

---
+++ Programmers/LEVEL0/문자열의 뒤의 n글자.swift
@@ -0,0 +1,7 @@
+// MARK: - 문자열 뒤의 n글자
+/// 문자열 my_string과 정수 n이 매개변수로 주어질 때,
+/// my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+func solution(_ my_string: String, _ n: Int) -> String {
+    String(my_string[my_string.index(my_string.startIndex, offsetBy: my_string.count - n)...])
+}
