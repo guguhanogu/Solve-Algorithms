d4aaf863562cf97396c8e0d274e0c0f517542587
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-05-30 17:28:57 +0900

    [Chore] 풀이 주석 추가 | 글자 지우기.swift
    [Chore] 풀이 주석 추가 | 글자 지우기.swift

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.swift
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.swift
@@ -1,8 +1,12 @@
 func solution(_ my_string: String, _ indices: [Int]) -> String {
     
+    /// my_string을 배열로 변환해 인덱스에 접근할 수 있게 합니다.
     var result: [Character] = Array(my_string)
     
+    /// 배열로 변환한 my_string에서
+    /// indices의 원소에 해당하는 인덱스의 글자를 지우고
     for index in indices { result[index] = " " }
     
+    /// 이어 붙인 문자열을 return 합니다.
     return String(result).replacingOccurrences(of: " ", with: "")
 }
