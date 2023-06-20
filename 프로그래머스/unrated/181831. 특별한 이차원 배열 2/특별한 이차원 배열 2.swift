fc7854afafa154f92092b37a4757f1980c9028a7
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-06-20 16:43:10 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ arr: [[Int]]) -> Int {
+    
+    let n: Int = arr.count - 1
+    
+    for i in 0...n {
+        for j in 0...n {
+            if arr[i][j] != arr[j][i] { return 0 }
+        } 
+    }
+    
+    return 1
+}
