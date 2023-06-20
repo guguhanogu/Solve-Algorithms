61af440a849145bbfe2c10817b59521809da1872
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-06-20 18:15:28 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.swift
@@ -0,0 +1,12 @@
+func solution(_ date1: [Int], _ date2: [Int]) -> Int {
+    if date1[0] > date2[0] { return 0 }
+    else if date1[0] < date2[0] { return 1 }
+    else {
+        if date1[1] > date2[1] { return 0 }
+        else if date1[1] < date2[1] { return 1 }
+        else {
+            if date1[2] < date2[2] { return 1 }
+            else { return 0 }
+        }
+    }
+}
